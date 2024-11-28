a31644a6bb040d12f98e4ef3411fdf51feb23b0f
Wol Vic John C. Lim <wolvicjohn@gmail.com>
2024-11-29 00:18:52 +0800

    1

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -31,14 +31,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a42e402107757cce1536e773b6198744
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "c7d07576fe101e1392ac5a264adff79e",
+"flutter_bootstrap.js": "8e408ea05fa3cf3006c3aa45bbc6da6f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3d2fc2977ab9368b489f2f1293e9d9ab",
-"/": "3d2fc2977ab9368b489f2f1293e9d9ab",
-"main.dart.js": "1b0579b13b11ab10b43327c4742d1646",
+"index.html": "b47052387089a4b6d7920b911f044c0b",
+"/": "b47052387089a4b6d7920b911f044c0b",
+"main.dart.js": "3add9813c89ddc6a712cd2bfae075c23",
 "manifest.json": "da92f9cdf3b8b7c6f9c00fbae3c90b6b",
 "version.json": "bde583cee80f0384d900b95b422a0fad"};
 // The application shell files that are downloaded before a service worker can
